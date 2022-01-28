7ea951875a37c3a9bde2b1e8641abc82c526dd6e
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-01-28 11:48:52 +1100

    feat: add firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCk0rs-Mr9-OtnBxrCdhOTNLQjkie0b5ts",
+	authDomain: "project-netflix-simulate-react.firebaseapp.com",
+	projectId: "project-netflix-simulate-react",
+	storageBucket: "project-netflix-simulate-react.appspot.com",
+	messagingSenderId: "570846393704",
+	appId: "1:570846393704:web:e2aece3c8c21f6b3f552dd"
+};
